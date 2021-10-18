08dfb3452af2d6bb7c33b3c5d4525dcba753e7e7
Patricia <pmaceirasvarela@danielcastelao.org>
2021-10-18 11:15:19 +0200

    Se cambia el id de los elementos. El jugador clika el boton de jugar y se muestra en el layout y en el logcat

--- app/src/main/java/com/dam2a/simondice/MainActivity.kt
+++ app/src/main/java/com/dam2a/simondice/MainActivity.kt
@@ -2,10 +2,24 @@ package com.dam2a.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Asigno a la variable empezarJugar el id del boton jugar
+        val empezarJugar :Button = findViewById(R.id.jugar)
+
+        //Cuando clikas el botón se muestra la ronda
+        empezarJugar.setOnClickListener{
+            mostrarRonda()
+        }
+
     }
-}
+
+    }
+    private fun mostrarRonda(){
+         Log.d("Estado","Mostrando ronda")
+    }
